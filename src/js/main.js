f570e7678f68bffb63d19f4df7ca5a8a7da40103
Shujinko53 <shujinko53@yandex.ru>
2021-09-18 19:55:21 +0300

    Replacing images has been realized

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import modals from './modules/modals';
 import sliders from './modules/sliders';
 import showMoreSlides from './modules/showMoreStyles';
+import pictureSize from './modules/pictureSize';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -9,4 +10,5 @@ window.addEventListener('DOMContentLoaded', () => {
     sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
     sliders('.main-slider-item', 'vertical');
     showMoreSlides('.button-styles', '.styles-2');
+    pictureSize('.sizes-block');
 });
